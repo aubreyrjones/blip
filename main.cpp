8f867bc06fd13d84e77504d7d0efa1349c38443b
Aubrey Jones <ajones@flagshipbio.com>
2015-09-23 20:23:07 -0600

    Got a basic black window... woohoo.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	cout << "Hello, World!" << endl;
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+	GLFWwindow *window = glfwCreateWindow(1920, 1080, "blip", nullptr, nullptr);
+	if (!window){
+		return 0;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)){
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window)){
+		glfwPollEvents();
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
+
 	return 0;
 }
